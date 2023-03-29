b6312a6e278e33bc739f7ab69c82613d29069be6
“mariam-garcia” <mariam.garcia@redux.co>
2023-03-29 14:30:23 -0400

    :sparkles: Added bottom tab navigator to hold home screeen and details along with the favorite screen :sparkles:

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,54 +3,52 @@ import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/home/HomeScreen';
 import MovieDetailsScreen from '../screens/details/MovieDetailsScreen';
 import {TRootStackParams} from '../types/NavigationProps';
-import BookMarkButton from '../components/buttons/favorite/BookMarkButton';
-import {Alert} from 'react-native';
-import {useDispatch} from 'react-redux';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import FavoriteScreen from '../screens/favorites/FavoriteScreen';
 
 const Stack = createStackNavigator<TRootStackParams>();
+const Tab = createBottomTabNavigator<TRootStackParams>();
 
+const HomeStack = () => {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Movies Inc',
+          headerStyle: {
+            backgroundColor: '#0d253f',
+          },
+          headerTintColor: 'white',
+        }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={MovieDetailsScreen}
+        options={{
+          headerStyle: {
+            backgroundColor: '#0d253f',
+          },
+          headerTintColor: 'white',
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const BottomTab = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeStack} />
+      <Tab.Screen name="Favorites" component={FavoriteScreen} />
+    </Tab.Navigator>
+  );
+};
 const Navigation = () => {
-  const style = {
-    right: 20,
-    top: 3,
-  };
-  const iconName = '';
-  const handleOnPress = () => {
-    Alert.alert('Pressed');
-  };
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            title: 'Movies Inc',
-            headerStyle: {
-              backgroundColor: '#0d253f',
-            },
-            headerTintColor: 'white',
-          }}
-        />
-        <Stack.Screen
-          name="Details"
-          component={MovieDetailsScreen}
-          options={{
-            headerStyle: {
-              backgroundColor: '#0d253f',
-            },
-            headerTintColor: 'white',
-            headerRight: () => (
-              <BookMarkButton
-                name={iconName}
-                style={style}
-                size={25}
-                onPress={handleOnPress}
-              />
-            ),
-          }}
-        />
-      </Stack.Navigator>
+      <BottomTab />
     </NavigationContainer>
   );
 };
